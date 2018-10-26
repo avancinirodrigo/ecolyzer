78b79fc98e941b08d0ae3fe023c163f955079637
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2018-10-26 17:15:48 -0300

    Close #5 - Save as csv.

--- gitdriller/git_analyzer.py
+++ gitdriller/git_analyzer.py
@@ -6,9 +6,9 @@ class GitAnalyzer:
 	def __init__(self, repo_path):
 		self.repo_path = repo_path
 		self.source_file_extensions = [
-			'c', 'cc', 'cpp', 'h', 'hpp', 'hxx', 
-			'ui', 'qrc', 
-			'lua', 
+			'c', 'cc', 'cpp', 'h', 'hpp', 'hxx',
+			'ui', 'qrc',
+			'lua',
 			'cmake', 'in',
 			'photo',
 			'sh',
@@ -19,7 +19,7 @@ class GitAnalyzer:
 		]
 		self.ignored_dirs = ['dependencies']
 		self.source_files = {}
-	
+
 	def set_tags(self, tags):
 		self.tags = tags
 
@@ -27,46 +27,59 @@ class GitAnalyzer:
 		for ignored in self.ignored_dirs:
 			if ignored in path:
 				return False
-				
+
 		return True
-		
+
 	def extract_added_source_files(self):
 		self.added_source_files = {}
-		
+
 		for i in range(1, len(self.tags)):
 			self.added_source_files[self.tags[i]] = PyDriller().get_added_files_between_tags(
-														self.repo_path, 
+														self.repo_path,
 														self.tags[i-1], self.tags[i]
-													)	
+													)
 		self.set_source_files()
-		
+
 	def set_source_files(self):
 		for tag in self.added_source_files:
 			sources = []
 			for i in range(0, len(self.added_source_files[tag])):
 				source_file = self.added_source_files[tag][i]
-				if (source_file.ext in self.source_file_extensions and 
+				if (source_file.ext in self.source_file_extensions and
 						self.is_valid_dir(source_file.path)):
 					sources.append(self.added_source_files[tag][i])
-			
+
 			self.source_files[tag] = sources
-	
+
+	def csv_str(self, tag, file_number, source_file):
+		return (tag + "," + str(file_number) + ","
+					+ source_file.fullpath + ","
+					+ source_file.ext + ","
+					+ str(source_file.added))
+
 	def show_csv(self):
 		files_count = 0
 		for tag in self.source_files:
 			for i in range(0, len(self.source_files[tag])):
 				source_file = self.source_files[tag][i]
 				files_count += 1
-				print(tag + "," + str(files_count) + "," 
-						+ source_file.fullpath + "," 
-						+ source_file.ext + "," 
-						+ str(source_file.added))	
-						
+				print(self.csv_str(tag, files_count, source_file))
+
+	def save_csv(self, filename):
+		file = open(filename, 'w')
+		files_count = 0
+		for tag in self.source_files:
+			for i in range(0, len(self.source_files[tag])):
+				source_file = self.source_files[tag][i]
+				files_count += 1
+				file.write(self.csv_str(tag, files_count, source_file) + '\n')
+		file.close()
+
 	def save(self, filename):
 		file = open(filename, 'wb')
 		pickle.dump(self.source_files, file)
 		file.close()
-	
+
 	def load(self, filename):
 		file = open(filename, 'rb')
 		self.source_files = pickle.load(file)
--- gitdriller/pydriller.py
+++ gitdriller/pydriller.py
@@ -6,18 +6,17 @@ class PyDriller(object):
 		if not hasattr(cls, '_instance'):
 			cls._instance = super(PyDriller, cls).__new__(cls, *args, **kwargs)
 		return cls._instance
-		
+
 	def get_added_files_between_tags(self, repo_path, from_tag, to_tag):
 		git_files = []
-		for commit in RepositoryMining(repo_path, only_in_branches=['master'], 
-									only_no_merge=True, from_tag=from_tag, 
+		for commit in RepositoryMining(repo_path, only_in_branches=['master'],
+									only_no_merge=True, from_tag=from_tag,
 									to_tag=to_tag).traverse_commits():
 			for m in commit.modifications:
 				if m.change_type != None:
-					git_file = GitFile(m.filename)
-					if ((m.change_type.name == "ADD") and (m.added > 0)):	
+					if ((m.change_type.name == "ADD") and (m.added > 0)):
+						git_file = GitFile(m.filename)
 						git_file.added = m.added
-						git_files.append(git_file)												
-		
+						git_files.append(git_file)
+
 		return git_files
-		
