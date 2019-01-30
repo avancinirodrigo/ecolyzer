96f7169447e13c35c0d20d21e638b326ecd20fd6
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2019-01-29 22:00:29 -0200

    Small correction #29.

--- tests/integration/sqlalchemy_test.py
+++ tests/integration/sqlalchemy_test.py
@@ -3,18 +3,15 @@ from ecolyzer.dataaccess import SQLAlchemyEngine
 
 def test_createdb():
 	url = 'postgresql://postgres:postgres@localhost:5432/createdb'
-	#SQLAlchemyEngine().create_engine(url)
 	SQLAlchemyEngine().createdb(url, True)
 	assert SQLAlchemyEngine().existsdb(url)
 	SQLAlchemyEngine().dropdb(url)
 
 def test_createdb_dbexists():
 	url = 'postgresql://postgres:postgres@localhost:5432/createdb'
-	#SQLAlchemyEngine().create_engine(url)
 	SQLAlchemyEngine().createdb(url, True)
 	with pytest.raises(Exception) as e:
 		SQLAlchemyEngine().createdb(url, False)
 	assert (('Database \'createdb\' already exists.')
 			in str(e.value))
 	SQLAlchemyEngine().dropdb(url)
-
