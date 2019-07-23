dd71a5940b06da02e1dc5fbfddf65f8d084d0764
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2019-07-22 22:28:41 -0300

    Review author repository relation #45.

--- ecolyzer/repository/author.py
+++ ecolyzer/repository/author.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from ecolyzer.dataaccess import Base
 
 class Author(Base):
@@ -6,8 +7,10 @@ class Author(Base):
 	__tablename__ = 'author'
 
 	id = Column(Integer, primary_key=True)
-	name = Column(String, nullable=False, unique=True)
-	email = Column(String, nullable=False, unique=True)
+	name = Column(String, nullable=False)
+	email = Column(String, nullable=False)
+	repository_id = Column(Integer, ForeignKey('repository.id'))
+	repository = relationship('Repository', backref=backref('author'))	
 
 	def __init__(self, name, email):
 		self.name = name
