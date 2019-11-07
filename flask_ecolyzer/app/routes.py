c51ca91b8e26e1103f4e9c4009ccdd07e217a80d
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2019-11-07 17:25:09 -0300

    refactoring #56.

--- flask_ecolyzer/app/routes.py
+++ flask_ecolyzer/app/routes.py
@@ -1,54 +1,43 @@
 from flask import jsonify, render_template, url_for
 import json
 from app import app, db
-from ecolyzer.repository import Author #Repository, Author, Person.
-#from ecolyzer.ecosystem import Ecosystem
+from ecolyzer.repository import Author
 from ecolyzer.ecosystem import Relationship
 
 @app.route('/authors')
 def authors():
 	authors = db.session.query(Author).all()
-	#resp = ''
-	#for a in authors:
-	#	resp += '<p>' + a.person.name + ': ' + a.person.email + '</p>'
-
-	#return resp
 	return render_template('authors.html', authors=authors)
 
 @app.route('/relationships', methods=['GET'])
 def relationships():
 	relations = db.session.query(Relationship).all()
 	to_system = relations[0].to_system.name
-	file_relations = {}
+	relations_count = []
+	source_pos = {}
 	for rel in relations:
-		to_code_element = rel.to_code_element
-		#key = to_code_element.source_file.fullpath()
-		key = to_code_element.source_file.id 
-		if key not in file_relations:
-			file_relations[key] = {}
-			file_relations[key]['id'] = rel.id
-			file_relations[key]['source'] = to_code_element.source_file.name()	
-			file_relations[key]['path'] = to_code_element.source_file.path()	
-			file_relations[key]['count'] = 0		
-			file_relations[key]['url'] = url_for('relationships', id=key)		
-		#rel_dict = row_to_dict(rel)
-		#rel_dict = {}
-		#rel_dict.source = str(to_code_element.source_file.name())
-		#rel_dict.id = rel.id
-		
-		#file_relations[to_code_element.source_file.fullpath()].append(rel_dict)
-		file_relations[key]['count'] = file_relations[key]['count'] + 1
-		#file_relations[to_code_element.source_file.fullpath()].append(rel)
+		source_id = rel.to_source_file_id
+		if source_id in source_pos:
+			pos = source_pos[source_id]
+			relations_count[pos]['count'] = relations_count[pos]['count'] + 1
+		else:
+			source_pos[source_id] = len(relations_count)
+			info = {
+				'id': rel.to_source_file_id,
+				'source': rel.to_source_file.name(),
+				'url': url_for('relationships', id=source_id),
+				'system': rel.to_system.name,
+				'count': 1
+			}
+			relations_count.append(info)
 
-	#return json.dumps(file_relations)
-	return render_template('relationships.html', relations=file_relations,
+	return render_template('relations_count.html', relations=relations_count,
 						system=to_system)
-	#return jsonify(relations=file_relations)
 
 @app.route('/relationships/<int:id>', methods=['GET'])
 def get_relationship(id):
 	relations = db.session.query(Relationship).filter_by(to_source_file_id = id).all()
-	source_file = relations[0].to_source_file #db.session.query(SourceFile).get(id)
+	source_file = relations[0].to_source_file
 	source_relations = []
 	from_source_pos = {}
 	from_systems = {}
@@ -70,10 +59,4 @@ def get_relationship(id):
 			source_relations.append(info)
 
 	return render_template('source_relations.html', relations=source_relations,
-						source_file=source_file.name(), from_systems=from_systems) #jsonify(row_to_dict(rel))
-
-def row_to_dict(row):
-	d = {}
-	for column in row.__table__.columns:
-		d[column.name] = str(getattr(row, column.name))
-	return d
+						source_file=source_file.name(), from_systems=from_systems)
