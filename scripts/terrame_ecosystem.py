b8180953971cef2f0b18abf86c4409ef38a3e533
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2020-01-17 15:41:56 -0300

    add all terrame ecosystem.

--- scripts/terrame_ecosystem.py
+++ scripts/terrame_ecosystem.py
@@ -4,7 +4,7 @@ from ecolyzer.repository import Repository, Person, Author, RepositoryMiner, Git
 from ecolyzer.dataaccess import SQLAlchemyORM
 from ecolyzer.ecosystem import Ecosystem
 
-db_url = 'postgresql://postgres:postgres@localhost:5432/ecosystem_last_commits'
+db_url = 'postgresql://postgres:postgres@localhost:5432/terrame_ecosystem'
 db = SQLAlchemyORM(db_url)
 db.create_all(True)
 session = db.create_session()
@@ -18,6 +18,10 @@ session.add(sys1)
 miner = RepositoryMiner(repo1, sys1)
 miner.add_ignore_dir_with('test')
 miner.add_ignore_dir_with('example')	
+miner.add_ignore_dir_with('data')	
+miner.add_ignore_dir_with('images')	
+miner.add_ignore_dir_with('luadoc')	
+miner.add_ignore_dir_with('zerobrane')	
 miner.extract_last_commits(session, '2.0-RC-8')
 
 repo2 = Repository('repo/ca')
@@ -55,6 +59,31 @@ sys8 = System('sci', repo8)
 miner = RepositoryMiner(repo8, sys8)	
 miner.extract_last_commits(session)
 
+repo9 = Repository('repo/urban')
+sys9 = System('urban', repo9)
+miner = RepositoryMiner(repo9, sys9)	
+miner.extract_last_commits(session)
+
+repo10 = Repository('repo/ford')
+sys10 = System('ford', repo10)
+miner = RepositoryMiner(repo10, sys10)	
+miner.extract_last_commits(session)
+
+repo11 = Repository('repo/sysdyn')
+sys11 = System('sysdyn', repo11)
+miner = RepositoryMiner(repo11, sys11)	
+miner.extract_last_commits(session)
+
+repo12 = Repository('repo/luccme')
+sys12 = System('luccme', repo12)
+miner = RepositoryMiner(repo12, sys12)	
+miner.extract_last_commits(session)
+
+repo13 = Repository('repo/inpeem')
+sys13 = System('impeem', repo13)
+miner = RepositoryMiner(repo13, sys13)	
+miner.extract_last_commits(session)
+
 ecosystem = Ecosystem()
 
 ecolyzer = EcosystemAnalyzer(ecosystem)
@@ -65,5 +94,10 @@ ecolyzer.make_relations(sys5, sys1, session)
 ecolyzer.make_relations(sys6, sys1, session)
 ecolyzer.make_relations(sys7, sys1, session)
 ecolyzer.make_relations(sys8, sys1, session)
+ecolyzer.make_relations(sys9, sys1, session)
+ecolyzer.make_relations(sys10, sys1, session)
+ecolyzer.make_relations(sys11, sys1, session)
+ecolyzer.make_relations(sys12, sys1, session)
+ecolyzer.make_relations(sys13, sys1, session)
 
 session.close()
