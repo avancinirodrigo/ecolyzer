ed55646916ed0a4e292d2fc36ea41246a3efb09f
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2020-02-17 14:53:32 -0300

    missing config #64.

--- gcloud/gcloud_conf.sh
+++ gcloud/gcloud_conf.sh
@@ -3,3 +3,6 @@
 cp -r ../flask_ecolyzer .
 cp app.yaml flask_ecolyzer
 cp -r ../ecolyzer flask_ecolyzer
+
+cp ../requirements.txt flask_ecolyzer
+cat ../flask_ecolyzer/requirements.txt >> flask_ecolyzer/requirements.txt
