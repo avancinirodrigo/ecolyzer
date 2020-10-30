26f103d6d91bcb5300620f191fac83b5c96776ed
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2020-10-30 20:08:28 -0300

    adding default constructor #78.

--- ecolyzer/parser/java_parser.py
+++ ecolyzer/parser/java_parser.py
@@ -19,7 +19,7 @@ class JavaParser():
 		for c in classes:
 			class_operations.append({'name': c.name,
 								 'operations': self._extract_declarations(c),
-								 'modifiers': self._get_method_modifier(c)})
+								 'modifiers': self._get_modifier(c)})
 		return class_operations
 
 	def extract_calls(self):
@@ -37,15 +37,14 @@ class JavaParser():
 	def _extract_methods(self):
 		methods = []
 		for path, node in self.tree.filter(javalang.tree.MethodDeclaration):
-			modifiers = self._get_method_modifier(node)
+			modifiers = self._get_modifier(node)
 			methods.append({'name': node.name, 'modifiers': modifiers})
 		return methods
 
-	def _get_method_modifier(self, node):
+	def _get_modifier(self, node):
 		modifiers = {}
 		for modifier in node.modifiers:
 			modifiers[modifier] = modifier
-
 		return modifiers
 
 	def _extract_invocations(self):
@@ -60,15 +59,24 @@ class JavaParser():
 			annotations.append(node.name)
 		return annotations
 
-	def _extract_declarations(self, tree):
+	def _extract_declarations(self, clas):
 		declarations = []
-		for path, node in tree.filter(javalang.tree.Declaration):
-			if (isinstance(node, javalang.tree.AnnotationDeclaration)
-				or isinstance(node, javalang.tree.MethodDeclaration)
-				or isinstance(node, javalang.tree.ConstructorDeclaration)):
-				modifiers = self._get_method_modifier(node)
+		has_constructor = False
+		for path, node in clas.filter(javalang.tree.Declaration):
+			if isinstance(node, javalang.tree.MethodDeclaration):
+				modifiers = self._get_modifier(node)
 				declarations.append({'name': node.name, 'modifiers': modifiers})
+			elif (isinstance(node, javalang.tree.ConstructorDeclaration)
+					or isinstance(node, javalang.tree.AnnotationDeclaration)):
+				has_constructor = True
+				modifiers = self._get_modifier(node)
+				declarations.append({'name': node.name, 'modifiers': modifiers})
+		if not has_constructor:
+			declarations.append(self._default_constructor(clas))	
 		return declarations		
+
+	def _default_constructor(self, clas):
+		return {'name': clas.name, 'modifiers': {'public': 'public'}}
 
 	def _walk_to_classes(self, children, classes):
 		if isinstance(children, list):
@@ -77,8 +85,7 @@ class JavaParser():
 					if isinstance(child, list):
 						self._walk_to_classes(child, classes)
 					elif isinstance(child, javalang.tree.Node):
-						if (isinstance(child, javalang.tree.ClassDeclaration)
-								or isinstance(child, javalang.tree.AnnotationDeclaration)):
+						if isinstance(child, javalang.tree.TypeDeclaration):
 							classes.append(child)
 					else:
 						raise NotImplementedError('Experimental exception.')
