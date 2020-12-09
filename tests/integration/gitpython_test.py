72543365cdaf424bd9ca9f40351cd048890a0477
Rodrigo Avancini <avancinirodrigo@yahoo.com.br>
2020-12-09 16:04:20 -0300

    trying to use clone a tag.

--- tests/integration/gitpython_test.py
+++ tests/integration/gitpython_test.py
@@ -1,7 +1,7 @@
 import os
 from os import path
 import shutil
-from ecolyzer.repository import GitPython
+from ecolyzer.repository import GitPython, Repository
 
 
 def test_clone():
@@ -23,3 +23,14 @@ def test_clone_branch_default_not_master():
 	git.clone('https://github.com/danfickle/openhtmltopdf', to_path)
 	assert GitPython.IsGitRepo(to_path)
 	shutil.rmtree(to_path)
+
+# def test_clone_tag(): #TODO
+# 	git = GitPython()
+# 	to_path = 'scribejava'
+# 	if path.exists(to_path):
+# 		shutil.rmtree(to_path, ignore_errors=True)
+
+# 	git.clone('https://github.com/scribejava/scribejava', to_path, branch='scribejava-8.0.0')
+# 	repo = Repository(to_path)
+# 	assert repo.branch == ''
+# 	shutil.rmtree(to_path)	
